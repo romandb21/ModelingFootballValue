1dda79ba10c22e04b966b9180cd4f65c51819604
rduprazbardou-ensae <roman.dupraz--bardou@ensae.fr>
2024-11-02 10:57:55 +0000

    free statbomb data l1 21/22 & 22/23

---
+++ teststatbomb.py
@@ -0,0 +1,20 @@
+#import packages
+from statsbombpy import sb
+import pandas as pd
+from mplsoccer import Pitch
+from mplsoccer import VerticalPitch,Pitch
+
+from highlight_text import ax_text, fig_text
+from matplotlib.colors import LinearSegmentedColormap
+import matplotlib.pyplot as plt
+import matplotlib.patheffects as path_effects
+import seaborn as sns
+
+#call statsbombpy API to get all free competitions
+free_comps = sb.competitions()
+
+#select two seasons of ligue 1 (2021/2022 and 2022/2023)
+ligue1_22_23 = sb.matches(competition_id=7, season_id=235)
+ligue1_21_22 = sb.matches(competition_id=7, season_id=108)
+
+print(ligue1_21_22.axes)
