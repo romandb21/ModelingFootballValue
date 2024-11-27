4544c3443aa28e1544a288e58a2f50928eda193f
romandb <roman.dupraz--bardou@ensae.fr>
2024-11-27 10:30:13 +0000

    getkaggle data changes (description of transfer fee and market values distribution)

--- getkaggledata.py
+++ getkaggledata.py
@@ -1,8 +1,8 @@
 import os
 import pandas as pd
-
+from math import sqrt
 # Définir le chemin du dossier où les fichiers CSV doivent être téléchargés
-output_dir = "~/work/ModelingFootballValue"
+output_dir = "/home/onyxia/work/ModelingFootballValue"
 
 # Assurez-vous que le répertoire existe, sinon créez-le
 os.makedirs(output_dir, exist_ok=True)
@@ -11,6 +11,8 @@ os.makedirs(output_dir, exist_ok=True)
 os.environ['KAGGLE_CONFIG_DIR'] = os.path.expanduser("~/.kaggle")
 
 # Télécharger les données depuis Kaggle
+
+
 def download_kaggle_dataset():
     dataset_name = "davidcariboo/player-scores"
     
@@ -19,6 +21,8 @@ def download_kaggle_dataset():
     print("Téléchargement terminé.")
 
 # Charger les données dans un DataFrame
+
+
 def load_data(file_name):
     file_path = os.path.join(output_dir, file_name)
     if os.path.exists(file_path):
@@ -27,10 +31,40 @@ def load_data(file_name):
         print(f"Le fichier {file_name} n'existe pas.")
         return None
 
-
 # Téléchargement et chargement des données
-download_kaggle_dataset()
-players_df = load_data("/home/onyxia/work/ModelingFootballValue/players.csv")
 
-if players_df is not None:
-    print(players_df.head())
+
+already_downloaded = True
+if already_downloaded is False:
+    download_kaggle_dataset()
+players = load_data("/home/onyxia/work/ModelingFootballValue/players.csv")
+transfers = load_data("/home/onyxia/work/ModelingFootballValue/transfers.csv")
+valuations = load_data("/home/onyxia/work/ModelingFootballValue/player_valuations.csv")
+
+     
+# Select only relevant transfers
+
+transfers_rel = transfers[transfers["transfer_fee"] >= 100000]
+
+# Differences between transfermarkt market values and transfer fees
+
+
+def diff_mean(df, v1, v2):
+    return abs(df[v1].mean() - df[v2].mean())
+
+
+def dist_l1(df, v1, v2):
+    return (abs(df[v1] - df[v2])).mean()
+
+
+def dist_l2(df, v1, v2):
+    return sqrt(((df[v1] - df[v2])**2).sum())
+
+
+def diff_distribution(df, v1, v2):
+    print(v1, "mean is", str(df[v1].mean()), "and variance is", str(df[v1].var()))
+    print(v2, "mean is", str(df[v2].mean()), "and variance is", str(df[v2].var()))
+    print("Absolute difference of their means is", str(diff_mean(df, v1, v2)), ", the mean absolute error is", str(dist_l1(df, v1, v2)), "and the root mean squared error is", str(dist_l2(df, v1, v2)))
+
+
+diff_distribution(transfers_rel, "transfer_fee", "market_value_in_eur")
