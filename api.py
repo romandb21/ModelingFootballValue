8a8875caed80ea35f814ece2396e885c994f40d7
paull <paul.lemoine12@orange.fr>
2024-12-21 23:33:00 +0100

    21/12

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.patheffects as path_effects
 
 # Details for the utilization of API
 API_KEY = "7fee06d41d0344a7b06b6907341c8a2b"
@@ -73,17 +74,24 @@ def draw_team(team_id):
     players_df = get_players(team_id)
     
     role_positions = {
-        "Goalkeeper" : [(50,98-i*2) for i in range(10)],
-        "Right-Back" : [(15,70-i*2) for i in range(10)],
-        "Centre-Back" :[(35 + (30 if i > 2 else 0), 80-(i*2 if i<2 else (i%3)*2)) for i in range(10)],
-        "Left-Back" : [(85,70-i*2) for i in range(10)],
-        "Central Midfield" :[(30,45-i*2) for i in range(10)],
-        "Defensive Midfield" : [(50,60-i*2) for i in range(10)],
-        "Attacking Midfield" : [(70,45-i*2) for i in range(10)],
-        "Right Winger" : [(20,25-i*2) for i in range(10)],
-        "Centre-Forward" : [(50,20-i*2) for i in range(10)],
-        "Left Winger" : [(80,25-i*2) for i in range(10)], }
+        "Goalkeeper" : [(50,98-i*3) for i in range(10)],
+        "Right-Back" : [(15,70-i*3) for i in range(10)],
+        "Centre-Back" :[(35 + (30 if i > 2 else 0), 80-(i*3 if i<2 else (i%3)*3)) for i in range(10)],
+        "Left-Back" : [(85,70-i*3) for i in range(10)],
+        "Central Midfield" :[(30,45-i*3) for i in range(10)],
+        "Defensive Midfield" : [(50,60-i*3) for i in range(10)],
+        "Attacking Midfield" : [(70,45-i*3) for i in range(10)],
+        "Right Winger" : [(20,25-i*3) for i in range(10)],
+        "Centre-Forward" : [(50,20-i*3) for i in range(10)],
+        "Left Winger" : [(80,25-i*3) for i in range(10)], }
     
+    positions_title = pd.DataFrame({
+    "name": ["Goalkeeper", "Right-Back", "Centre-Back", "Centre-Back", "Left-Back", 
+             "Central Midfield", "Defensive Midfield", "Attacking Midfield", "Right Winger", 
+             "Centre-Forward", "Left Winger"],
+    "x": [50, 15, 35, 65, 85, 30, 50, 70, 20, 50, 80], 
+    "y": [101, 73, 83, 83, 73, 48, 63, 48, 28, 23, 28]   
+    })
     
     positions = []
     position_counters = {role: 0 for role in role_positions}
@@ -92,14 +100,14 @@ def draw_team(team_id):
         role = player["Position"]
         if role in role_positions:
             counter = position_counters[role]
-            if counter < len(role_positions[role]):  # Vérifie si une position est disponible
+            if counter < len(role_positions[role]):  
                 pos = role_positions[role][counter]
                 position_counters[role] += 1
                 positions.append({"name": player["Player Name"], "x": pos[0], "y": pos[1]})
     
     position_df = pd.DataFrame(positions)
     
-    plt.figure(figsize=(8, 12)).patch.set_facecolor("green")  # Dimensions of the pitch
+    plt.figure(figsize=(8, 12)).patch.set_facecolor("olivedrab")  # Dimensions of the pitch
     plt.plot([0, 100], [0, 0], color="white") # All the lines of the pitch 
     plt.plot([0, 0], [0, 100], color="white")  
     plt.plot([100, 100], [0, 100], color="white")  
@@ -116,13 +124,28 @@ def draw_team(team_id):
     plt.xlim(0, 100)
     plt.ylim(0, 100)
     plt.axis("off")  # hide axes
+    
+    title=plt.text( 50, 108, player["Team Name"], color="darkkhaki", fontsize=18, ha="center", va="center" )
+    title.set_path_effects([
+            path_effects.Stroke(linewidth=1.5, foreground="black"),  
+            path_effects.Normal()])
 
    
     for _, row in position_df.iterrows():
-        plt.text(row["x"], row["y"], row["name"], color="black", fontsize=8,
-                 ha="center", va="center")
+        plt.text(row["x"], row["y"], row["name"], color="white", fontsize=9,
+                 ha="center", va="center").set_path_effects([
+            path_effects.Stroke(linewidth=1.5 , foreground="black"),  
+            path_effects.Normal()])
+        
+    for _, row in positions_title.iterrows():
+        text=plt.text(row["x"], row["y"], row["name"], color="darkkhaki", fontsize=11,
+                 ha="center", va="center").set_path_effects([
+            path_effects.Stroke(linewidth=1.5, foreground="black"),  
+            path_effects.Normal()])
+ 
+       
 
-    
     plt.show()
 
-draw_team(521)
+
+draw_team(57)
