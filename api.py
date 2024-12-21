b5ad639db2e8f9f5524425a225c246dd41034efb
paull <paul.lemoine12@orange.fr>
2024-12-21 18:35:43 +0100

    Affichage équipe

--- api.py
+++ api.py
@@ -63,11 +63,42 @@ def get_players(team_id):
         })
     players_df = pd.DataFrame(players_list)
     print(players_df)
+    return players_df
 
 
-'''Define a function to plot a football pitch'''
+'''Define a function to plot a football team on the pitch'''
 
-def draw_pitch():
+def draw_team(team_id):
+    
+    players_df = get_players(team_id)
+    
+    role_positions = {
+        "Goalkeeper" : [(50,98-i*2) for i in range(10)],
+        "Right-Back" : [(15,70-i*2) for i in range(10)],
+        "Centre-Back" :[(35 + (30 if i > 2 else 0), 80-(i*2 if i<2 else (i%3)*2)) for i in range(10)],
+        "Left-Back" : [(85,70-i*2) for i in range(10)],
+        "Central Midfield" :[(30,45-i*2) for i in range(10)],
+        "Defensive Midfield" : [(50,60-i*2) for i in range(10)],
+        "Attacking Midfield" : [(70,45-i*2) for i in range(10)],
+        "Right Winger" : [(20,25-i*2) for i in range(10)],
+        "Centre-Forward" : [(50,20-i*2) for i in range(10)],
+        "Left Winger" : [(80,25-i*2) for i in range(10)], }
+    
+    
+    positions = []
+    position_counters = {role: 0 for role in role_positions}
+    
+    for _, player in players_df.iterrows():
+        role = player["Position"]
+        if role in role_positions:
+            counter = position_counters[role]
+            if counter < len(role_positions[role]):  # Vérifie si une position est disponible
+                pos = role_positions[role][counter]
+                position_counters[role] += 1
+                positions.append({"name": player["Player Name"], "x": pos[0], "y": pos[1]})
+    
+    position_df = pd.DataFrame(positions)
+    
     plt.figure(figsize=(8, 12)).patch.set_facecolor("green")  # Dimensions of the pitch
     plt.plot([0, 100], [0, 0], color="white") # All the lines of the pitch 
     plt.plot([0, 0], [0, 100], color="white")  
@@ -84,7 +115,14 @@ def draw_pitch():
     plt.gca().add_patch(circle)
     plt.xlim(0, 100)
     plt.ylim(0, 100)
-    plt.axis("off")  # Hide axes
+    plt.axis("off")  # hide axes
+
+   
+    for _, row in position_df.iterrows():
+        plt.text(row["x"], row["y"], row["name"], color="black", fontsize=8,
+                 ha="center", va="center")
+
+    
     plt.show()
 
-draw_pitch()
+draw_team(521)
